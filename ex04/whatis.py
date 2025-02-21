b00881d840024634f33d54649698c9b1becfd27b
Maaz Khan <maazkhan7454@gmail.com>
2025-02-21 04:59:30 +0400

    fixed some norm

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,19 +1,20 @@
 import sys
 
+
 def whatis(num: int) -> str:
     if num % 2 == 0:
         return "I'm Even."
     else:
         return "I'm Odd."
 
+
 try:
     assert len(sys.argv) <= 2, "more than one argument is provided"
-    try: 
+    try:
         if len(sys.argv) == 2:
             num = int(sys.argv[1])
-            print(whatis(int(sys.argv[1])))  
+            print(whatis(int(sys.argv[1])))
     except ValueError:
         raise AssertionError("argument is not an integer")
 except AssertionError as e:
     print(f"AssertionError: {e}")
-        
