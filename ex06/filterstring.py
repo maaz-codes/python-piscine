3079a20160bb7af1845ccf5cf5d7e69c66d6bef2
Maaz Khan <maazkhan7454@gmail.com>
2025-02-23 20:22:13 +0400

    learned dictionaries

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -6,7 +6,7 @@ def main():
         assert len(sys.argv) == 3, "the arguments are bad"
         S = str(sys.argv[1])
         N = int(sys.argv[2])
-        print(lambda S, N: word for word in S.split(' ') if len(word) > N)
+        print((lambda S, N: [word for word in S.split(' ') if len(word) > N])(S, N))
     except AssertionError as e:
         print(f"AssertionError: {e}")
     except ValueError:
---
+++ ex07/sos.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def morse_code_encoder(text: str) -> str:
+    NESTED_MORSE = {
+        'A': '.- ', 'B': '-... ', 'C': '-.-. ', 'D': '-.. ', 'E': '. ',
+        'F': '..-. ', 'G': '--. ', 'H': '.... ', 'I': '.. ', 'J': '.--- ',
+        'K': '-.- ', 'L': '.-.. ', 'M': '-- ', 'N': '-. ', 'O': '--- ',
+        'P': '.--. ', 'Q': '--.- ', 'R': '.-. ', 'S': '... ', 'T': '- ',
+        'U': '..- ', 'V': '...- ', 'W': '.-- ', 'X': '-..- ', 'Y': '-.-- ',
+        'Z': '--.. ', ' ': '/ ',
+        '0': '----- ', '1': '.---- ', '2': '..--- ', '3': '...-- ',
+        '4': '....- ', '5': '..... ', '6': '-.... ', '7': '--... ',
+        '8': '---.. ', '9': '----. ',
+        }
+    morse_code = ""
+    for c in text.upper():
+        if c not in NESTED_MORSE:
+            raise ValueError(f"char {c} not present in morse.")
+        morse_code += NESTED_MORSE[c]
+    return morse_code
+
+
+def main():
+    try:
+        assert (
+            len(sys.argv) == 2 and
+            sys.argv[1].replace(' ', '').isalnum()
+        ), "the arguments are bad"
+        print("From the main")
+        print(morse_code_encoder(str(sys.argv[1])))
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except ValueError as e:
+        print(f"ValueError: {e}")
+
+
+if __name__ == "__main__":
+    main()
