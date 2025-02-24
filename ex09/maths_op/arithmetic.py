62a727fc29efe431de3be5df8d5bbde3020cd06d
Maaz Khan <maazkhan7454@gmail.com>
2025-02-25 03:13:17 +0400

    learned about modules, packages, how to create them, packaging

---
+++ ex09/maths_op/arithmetic.py
@@ -0,0 +1,14 @@
+def add(x: int, y: int) -> str:
+    return f"adding... {x} and {y}"
+
+
+def substract(x: int, y: int) -> str:
+    return f"substracting... {x} from {y}"
+
+
+def multiply(x: int, y: int) -> str:
+    return f"multiplying... {x} with {y}"
+
+
+def divide(x: int, y: int) -> str:
+    return f"diving... {x} by {y}"
---
+++ ex09/tester.py
@@ -0,0 +1,12 @@
+from maths_op import arithmetic, logical
+
+
+def main():
+    print(arithmetic.add(2, 3))
+    print(arithmetic.substract(4, 1))
+    print(logical.and_op(5, 10))
+    print(logical.or_op(19, 2))
+
+
+if __name__ == '__main__':
+    main()
