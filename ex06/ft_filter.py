298158977cd864207eeb415bbe4e55c4e573b85e
Maaz Khan <maazkhan7454@gmail.com>
2025-02-25 04:21:06 +0400

    fixed None passing as arg in ft_filter

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,11 +1,14 @@
-from typing import Callable, Iterable, Iterator
+from typing import Callable, Iterable, Iterator, Optional
 
 
-def ft_filter(filter_func: Callable, iterable: Iterable) -> Iterator:
+def ft_filter(filter_func: Optional[Callable], iterable: Iterable) -> Iterator:
     """
-    Return an iterator yielding those items of iterable for which function(item) is true. 
+    Return an iterator yielding those items of iterable
+    for which function(item) is true.
     If function is None, return the items that are true.
     """
+    if filter_func is None:
+        return (iter for iter in iterable if iter)
     return (iter for iter in iterable if filter_func(iter))
 
 
@@ -15,9 +18,9 @@ def is_even(num: int) -> bool:
 
 def main():
     try:
-        numbers = [1, 2, 3, 4, 5, 6]
-        print(list(ft_filter(is_even, numbers)))
-        print(list(filter(is_even, numbers)))
+        numbers = [0, 1, 2, 3, 4, 5, 6]
+        print(list(ft_filter(None, numbers)))
+        print(list(filter(None, numbers)))
     except Exception as e:
         print(f"Error: {e}")
 
